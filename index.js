1e4f1bcc3c9fd49931f2ca90b6748cb95cca8878
Bas de Vaan <bas.de.vaan@kabisa.nl>
2020-09-22 14:22:59 +0200

    add calculating time from origin time

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = (str) => {
-    return `💩${str}💩`;
+module.exports.dateToBassiuzTime = (date) => {
+    return getBassiuzTimestamp(date);
+}
+module.exports.now = () => {
+    return getBassiuzTimestamp(new Date());
+}
+
+getBassiuzTimestamp = (date) => {
+    const originTime = new Date('1992 Nov 21');
+    originTime.setHours(20);
+    originTime.setMinutes(10);
+    originTime.setSeconds(10);
+    return date.getTime() - originTime.getTime();
 }
